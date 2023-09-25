726b4716a50c9864d31d55dc4599b3c8fb8f36b3
bastianl <bastian.lennart@gmail.com>
2023-09-25 15:52:41 +0200

    iterative solver with cvxpy layers

--- experiments/functional_correspondence/fmaps_model.py
+++ experiments/functional_correspondence/fmaps_model.py
@@ -3,12 +3,45 @@ import sys
 
 import torch
 import torch.nn as nn
+import cvxpy as cp
+from cvxpylayers.torch import CvxpyLayer
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../src/"))
 import diffusion_net
 
 
-def compute_correspondence(feat_x, feat_y, evals_x, evals_y, evecs_trans_x, evecs_trans_y, lambda_param=1e-3):
+def compute_correspondence_iterative(feat_x, feat_y, evals_x, evals_y, evecs_trans_x, evecs_trans_y, lambda_param=1e-3):
+    """
+    Computes the functional map correspondence matrix C given features from two shapes.
+
+    Has no trainable parameters.
+    """
+
+    feat_x, feat_y = feat_x.unsqueeze(0), feat_y.unsqueeze(0)
+    evecs_trans_x, evecs_trans_y = evecs_trans_x.unsqueeze(0), evecs_trans_y.unsqueeze(0)
+    evals_x, evals_y = evals_x.unsqueeze(0), evals_y.unsqueeze(0)
+
+    F_hat = torch.bmm(evecs_trans_x, feat_x)
+    G_hat = torch.bmm(evecs_trans_y, feat_y)
+    A, B = F_hat.squeeze(0), G_hat.squeeze(0)
+
+    C = cp.Variable((evals_x.size(1), evals_y.size(1)))
+    A_var = cp.Parameter((A.size(0), A.size(1)))
+    B_var = cp.Parameter((B.size(0), B.size(1)))
+    L1_var = cp.Parameter((evals_x.size(1)))
+    L2_var = cp.Parameter((evals_y.size(1)))
+    objective = cp.Minimize(cp.norm(C @ A_var - B_var, 'fro') +
+                            cp.norm(C @ cp.diag(L1_var) - cp.diag(L2_var) @ C, 'fro'))
+
+    problem = cp.Problem(objective)
+
+    cvxpylayer = CvxpyLayer(problem, parameters=[A_var, B_var, L1_var, L2_var], variables=[C])
+    C, = cvxpylayer(A, B, evals_x.squeeze(0), evals_y.squeeze(0))
+
+    return C.to(feat_x.device).unsqueeze(0)
+
+
+def compute_correspondence_explicit(feat_x, feat_y, evals_x, evals_y, evecs_trans_x, evecs_trans_y, lambda_param=1e-3):
     """
     Computes the functional map correspondence matrix C given features from two shapes.
 
@@ -78,6 +111,17 @@ class FunctionalMapCorrespondenceWithDiffusionNetFeatures(nn.Module):
         # construct the fmap
         evecs_trans1, evecs_trans2 = evecs1.t()[:self.n_fmap] @ torch.diag(mass1), evecs2.t()[:self.n_fmap] @ torch.diag(mass2)
         evals1, evals2 = evals1[:self.n_fmap], evals2[:self.n_fmap]
-        C_pred = compute_correspondence(feat1, feat2, evals1, evals2, evecs_trans1, evecs_trans2, lambda_param=self.lambda_param)
+        import time
+        t0 = time.time()
+        C_iterative= compute_correspondence_iterative(feat1, feat2, evals1, evals2, evecs_trans1, evecs_trans2, lambda_param=self.lambda_param)
+        print("iterative", time.time()-t0)
+        t0 = time.time()
+        C_explicit= compute_correspondence_explicit(feat1, feat2, evals1, evals2, evecs_trans1, evecs_trans2, lambda_param=self.lambda_param)
+        print("explicit", time.time() - t0)
+
+        diff = torch.mean(torch.abs(C_iterative - C_explicit))
+        print("diff: ", diff)
+
+        C_pred = C_iterative
 
         return C_pred, feat1, feat2
